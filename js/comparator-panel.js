4a56b0e087e2618f4990bba7e4d25d6f3c937210
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2012-03-10 17:39:48 +0000

    Circles are now sorted in multiple horizontal lines

--- js/comparator-panel.js
+++ js/comparator-panel.js
@@ -28,23 +28,23 @@
 
   methods.refresh = function () {
     var self = this,
-        step = 25,
-        startY = 50,
-        marginY = 10,
+        stepY = 25,
+        maxX = this.options.width,
+        maxY = this.options.height,
         data = this.data.sort(function (a, b) {
           return d3.descending(a.value, b && b.value);
         }),
-        cy = function (d, i) {
-          return i * step;
-        },
+        startY = 50,
+        marginY = 10,
+        marginX = 10,
         nodes = this.vis.selectAll("circle.node").data(this.data, function (d) { return d.value; }),
-        lastY = 0;
+        lastY = maxY / 2,
+        lastX = 0;
 
     nodes.order(data);
 
     nodes.enter().append('svg:circle')
       .attr("class", "node")
-      .attr("cx", function(d) { return 100; })
       .attr("r", function (d) { return d.r; })
       .style("fill", function(d, i) { return d.fill })
       .style("stroke", function(d, i) { return '#000'; })
@@ -52,9 +52,22 @@
 
     nodes.exit().remove();
 
-    nodes.transition().duration(500).attr('cy', function (d, i) {
-      return lastY += (d.r * 2) + marginY;
-    });
+    nodes.transition().duration(500)
+      .attr('cx', function (d, i) {
+        var step = (d.r * 2) + marginY,
+            cx = lastX = lastX + step;
+
+        // reset the value of x when it becomes 
+        // greater than the viewport width
+        if (cx + d.r >= maxY ) {
+          cx = lastX = step;
+          lastY += d3.max(data, function (d) { return d.r * 2; }) + marginY;
+        }
+        return cx;
+      })
+      .attr("cy", function(d) { 
+        return lastY;
+      })
 
   }
   _.extend(ComparatorPanel.prototype, this.Delegator.prototype, methods);
